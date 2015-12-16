505d25feeed83ea9c9643e1a280031e36b110282
Hubert <hpompecki@gmail.com>
2015-12-16 13:11:28 +0000

    implement Board#insert_token

--- lib/board.rb
+++ lib/board.rb
@@ -8,7 +8,7 @@ class Board
   
   def to_s
     string = "|1|2|3|4|5|6|7|\n"
-    0.upto(5) do |i|
+    5.downto(0) do |i|
       string << "|"
       0.upto(6) do |j|
         string << get_token_for_printing(i, j).to_s
@@ -19,10 +19,34 @@ class Board
     string
   end
   
+  def insert_token(column, token)
+    raise ArgumentError if !column.between?(1,7)
+    row = 0
+    until get_token(row, column).nil?
+      row += 1
+      raise FullColumnError if row > 5
+    end
+    set_token(row, column, token)
+  end
+  
   private
   
+    def get_token(row, column)
+      tokens[row * 7 + column - 1]
+    end
+    
+    def set_token(row, column, new_value)
+      tokens[row * 7 + column - 1] = new_value
+    end
+    
     def get_token_for_printing(i, j)
       tokens[i * 7 + j] || ' '
     end
+  
+  class FullColumnError < ArgumentError
+    def initialize(default_message = "Selected column is full!")
+      super(default_message)
+    end
+  end
   
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -38,4 +38,54 @@ END_STRING
     end
   end
   
+  describe '#insert_token' do
+    it "takes two arguments" do
+      expect { board.insert_token(2, "a token")  }.not_to raise_error
+      expect {  board.insert_token }.to raise_error ArgumentError
+    end
+    
+    it "throws ArgumentError when given invalid column number" do
+      expect { board.insert_token(0, "a token") }.to raise_error ArgumentError
+      expect { board.insert_token(8, "a token") }.to raise_error ArgumentError
+      expect { board.insert_token(3, "a token") }.not_to raise_error
+    end
+    
+    it "places the given token at the lowest free space in the given column" do
+      board.insert_token(1, "T")
+      expected_board = <<END_STRING
+|1|2|3|4|5|6|7|
+| | | | | | | |
+| | | | | | | |
+| | | | | | | |
+| | | | | | | |
+| | | | | | | |
+|T| | | | | | |
+END_STRING
+      expect(board.to_s).to eq expected_board
+    end
+    
+    it "places tokens at the lowest free space in the given column" do
+      board.insert_token(4, "A")
+      board.insert_token(4, "B")
+      board.insert_token(5, "Z")
+      board.insert_token(4, "C")
+      expected_board = <<END_STRING
+|1|2|3|4|5|6|7|
+| | | | | | | |
+| | | | | | | |
+| | | | | | | |
+| | | |C| | | |
+| | | |B| | | |
+| | | |A|Z| | |
+END_STRING
+      expect(board.to_s).to eq expected_board
+    end
+    
+    it "throws FullColumnError when the column is full" do
+      5.times { board.insert_token(2, "T") }
+      expect { board.insert_token(2, "T") }.not_to raise_error
+      expect { board.insert_token(2, "T") }.to raise_error Board::FullColumnError
+    end
+    
+  end
 end
